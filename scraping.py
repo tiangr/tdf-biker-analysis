00f561679536bf487687629004567ebd699c92ff
jantopolovec <jantopolovec7@gmail.com>
2025-04-09 20:15:32 +0200

    Total wins
    Statistika glede skupnih zmag, ter 1-3 mesto

--- scraping.py
+++ scraping.py
@@ -6,7 +6,8 @@ import io
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
+columns = ["Rider", "1st", "2nd", "3rd", "Total Wins"]
+stats_df = pd.DataFrame(columns=columns)
 
 def time_to_seconds(time_str):
     parts = time_str.split(":")
@@ -24,6 +25,37 @@ def time_to_seconds(time_str):
     
     return hours * 3600 + minutes * 60 + seconds
 
+def create_stats_file(sorted_stats):
+    sorted_stats.to_csv("tdf_stats.csv", index=False, header=True, sep=";")
+
+def add_new_rider(rider):
+    # Add a new rider to the stats_df DataFrame
+    global stats_df
+    new_row = pd.DataFrame({"Rider": [rider], "1st": [0], "2nd": [0], "3rd": [0], "Total Wins": [0]})
+    stats_df = pd.concat([stats_df, new_row], ignore_index=True)
+
+def add_podiums(table):
+    # Add podiums to the stats_df DataFrame
+    global stats_df
+    for i in range(3):
+        rider = table.loc[i, "Rider"]
+        if rider not in stats_df["Rider"].values:
+            add_new_rider(rider)
+        stats_df.loc[stats_df["Rider"] == rider, columns[i+1]] += 1
+
+def get_overall_winner(response):
+    # Extract the overall winner from the response
+    global stats_df
+    try:
+        overall_winner = pd.read_html(io.StringIO(response))[1]
+        overall_winner = overall_winner.loc[0, "Rider"].replace(f" {overall_winner.loc[0, 'Team']}", "")
+    except:
+        return
+
+    if overall_winner not in stats_df["Rider"].values:
+        add_new_rider(overall_winner)
+    stats_df.loc[stats_df["Rider"] == overall_winner, "Total Wins"] += 1
+
 G = nx.MultiDiGraph()
 edition = "a"
 for year in range(1903,2025):
@@ -37,6 +69,9 @@ for year in range(1903,2025):
         no_stages = int(no_stages)
     except:
         continue
+
+    # Get the overall winner
+    get_overall_winner(response)
 
     for stage in range(1,no_stages+1):
         stage_url = f"https://www.procyclingstats.com/race/tour-de-france/{year}/stage-{stage}"
@@ -76,6 +111,10 @@ for year in range(1903,2025):
         table = table.drop(columns=["Team","Rnk"])
         table = table.reset_index(drop=True)
         
+        # Adding podiums
+        if len(table) >= 3:
+            add_podiums(table)
+        
         # Constructing/Adding to - Multigraph
         for i, row in table.iterrows():
             G.add_node(row["Rider"], time=row["Time"])
@@ -90,3 +129,7 @@ for year in range(1903,2025):
                 weight = max(current_time - prev_time, 1)
                 G.add_edge(current_rider, prev_rider, weight=weight)
 nx.write_pajek(G, f"TDF")
+
+# Sort the DataFrame by Total Wins, 1st, 2nd, and 3rd podiums
+stats_df = stats_df.sort_values(by=["Total Wins", "1st", "2nd", "3rd"], ascending=False).reset_index(drop=True)
+create_stats_file(stats_df)
